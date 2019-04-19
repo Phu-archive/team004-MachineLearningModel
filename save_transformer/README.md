7b65a1b156cf87941af064f3a241e36a58cfc39d
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2019-04-19 23:19:44 +0100

    Adding Transformer Downloading file

--- save_transformer/README.md
+++
@@ -1,1 +0,0 @@
-# Getting the Checkpoint Files
---
+++ save_transformer/dowload_transformer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Downloading the Dataset"
+wget https://www.dropbox.com/s/eb6mfwu166dbmon/trained.zip?dl=0
+unzip trained.zip?dl=0
+rm trained.zip?dl=0
